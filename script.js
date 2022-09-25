5630b94ba3ca06220527ed20ee18e1604114ff45
Dhiraj <dhiraj7631090066@gmail.com>
2022-09-25 16:18:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const showBtn = document.querySelector('.navBtn');
+const topNav = document.querySelector('.top-nav');
+
+showBtn.addEventListener('click', function(){
+    if(topNav.classList.contains('showNav')){
+        topNav.classList.remove('showNav');
+        showBtn.innerHTML = '<i class = "fas fa-bars"></i>';
+    } else {
+        topNav.classList.add('showNav');
+        showBtn.innerHTML = '<i class = "fas fa-times"></i>';
+    }
+});
+
+/* Lightbox */
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "dhiraj7631090066@gmail.com",
+        Password : "15Mm2000@#123",
+        To : 'dhiraj7631090066@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New Contact Form Enquiry",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
